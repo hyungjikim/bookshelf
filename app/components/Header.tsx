890bfc360893253145095d8fc3766592b11b4479
hyungji.k <icthelight@naver.com>
2025-04-19 16:14:47 +0900

    ✨ #3 feat: 글로벌 헤더

---
+++ app/components/Header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import * as stylex from "@stylexjs/stylex";
+
+export default function Header() {
+  return (
+    <header>
+      <div {...stylex.props(styles.wrapper)}>
+        <div>
+          <Image src="/tango.webp" alt="Logo" width={100} height={100} />
+        </div>
+        <nav>
+          <ul {...stylex.props(styles.list)}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+const styles = stylex.create({
+  wrapper: {
+    display: "grid",
+    gridTemplateColumns: "auto 1fr",
+  },
+  list: {
+    height: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    alignItems: "center",
+    listStyle: "none",
+  },
+});
