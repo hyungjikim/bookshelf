39e8ad33204723316f9b78d78a98228bc65339de
hyungji.k <icthelight@naver.com>
2025-04-22 19:48:58 +0900

    ✨ #16 feat: supabase 로oauth (google, github) 구현

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,21 @@
-import { login, signup } from "./actions";
+import { login, signup, socialLogin } from "./actions";
 
 export default function LoginPage() {
+  const googleLogin = socialLogin.bind(null, "google");
+  const githubLogin = socialLogin.bind(null, "github");
+
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+      <button onClick={googleLogin}>login with google</button>
+      <button onClick={githubLogin}>login with github</button>
+    </>
   );
 }
