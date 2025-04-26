c80aeaa515bcc05a2b8746138d4fade3f10712fa
hyungji.k <icthelight@naver.com>
2025-04-26 21:35:17 +0900

    ✨ #17 feat: 미들웨어에 sign-up 추가

--- supabase/middleware.ts
+++ supabase/middleware.ts
@@ -57,7 +57,7 @@ export async function updateSession(request: NextRequest) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/login";
-    return NextResponse.redirect(url);
+    // return NextResponse.redirect(url);
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
