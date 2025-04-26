59d93ab6438b1f0e4b6d84cffa464b2283a8e8ee
hyungji.k <icthelight@naver.com>
2025-04-26 21:42:20 +0900

    🔥 #17 chore: 사용하지 않는 코드 제거

--- app/(without-layout)/layout.tsx
+++ app/(without-layout)/layout.tsx
@@ -1,10 +1,5 @@
 import { ReactNode } from "react";
 
-export default function Layout({
-  children,
-}: {
-  children: ReactNode;
-  modal: ReactNode;
-}) {
+export default function Layout({ children }: { children: ReactNode }) {
   return children;
 }
