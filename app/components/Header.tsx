efb4d19241bf939a0a9821ed75e036ad468e4f3b
hyungji.k <icthelight@naver.com>
2025-04-26 21:35:04 +0900

    ✨ #17 feat: 네비게이션에 로그인 라우트 추가

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,6 +20,9 @@ export default function Header() {
             <li>
               <Link href="/contact">Contact</Link>
             </li>
+            <li>
+              <Link href="/login">로그인</Link>
+            </li>
           </ul>
         </nav>
       </div>
