c598ee47912404b5ee8f27b7de5f2df817874f08
hyungji.k <icthelight@naver.com>
2025-04-28 20:23:40 +0900

    📦 #33 feat: tiptap installation

---
+++ app/(without-layout)/new/components/Tiptap.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3],
+        },
+      }),
+    ],
+    content: "<p>Hello World! 🌎️</p>",
+  });
+
+  return <EditorContent editor={editor} />;
+};
+
+export default Tiptap;
