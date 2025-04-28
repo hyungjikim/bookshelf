f09816529fd924106320373d90f4e1d9139d9afc
hyungji.k <icthelight@naver.com>
2025-04-28 15:58:26 +0900

    ✨ #17 feat: 로그인 예외 처리

--- app/(without-layout)/sign-in/page.tsx
+++ app/(without-layout)/sign-in/page.tsx
@@ -1,20 +1,71 @@
+"use client";
+
 import Link from "next/link";
 import { signIn, socialLogin } from "./actions";
+import Form from "next/form";
+import { useActionState } from "react";
+import { State } from "./types";
+
+const initialState = {
+  fieldError: {},
+  globalError: "",
+};
 
 export default function SignInPage() {
   const googleLogin = socialLogin.bind(null, "google");
   const githubLogin = socialLogin.bind(null, "github");
 
+  const [state, formAction, pending] = useActionState<State, FormData>(
+    signIn,
+    initialState
+  );
+
   return (
     <>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={signIn}>Log in</button>
-        <Link href="/sign-up">회원가입하기</Link>
-      </form>
+      <Form action={formAction}>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            defaultValue={state.inputs?.email}
+          />
+          {state.fieldError?.email && (
+            <p aria-live="polite" role="status">
+              {state.fieldError.email[0]}
+            </p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            defaultValue={state.inputs?.password}
+          />
+          {state.fieldError?.password && (
+            <p aria-live="polite" role="status">
+              {state.fieldError.password[0]}
+            </p>
+          )}
+        </div>
+
+        <button type="submit" disabled={pending}>
+          Log in
+        </button>
+        {state.globalError && (
+          <p aria-live="polite" role="status">
+            {state.globalError}
+          </p>
+        )}
+      </Form>
+      <Link href="/sign-up">회원가입하기</Link>
+
       <button onClick={googleLogin}>login with google</button>
       <button onClick={githubLogin}>login with github</button>
     </>
