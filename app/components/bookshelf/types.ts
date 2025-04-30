9aa79cdc19dd0f533f01e50855f99a908932d33b
hyungji.k <icthelight@naver.com>
2025-04-30 15:01:08 +0900

    🏷️ #45 chore: 책 타입 추출

---
+++ app/components/bookshelf/types.ts
@@ -0,0 +1,15 @@
+import { Database } from "@/database.types";
+
+type Book = Pick<
+  Database["public"]["Tables"]["books"]["Row"],
+  "title" | "author" | "publisher"
+>;
+
+type BookDetail = Pick<
+  Database["public"]["Tables"]["book_details"]["Row"],
+  "id" | "created_at"
+>;
+
+export type BooksListWithJoin = BookDetail & {
+  book_id: Book;
+};
