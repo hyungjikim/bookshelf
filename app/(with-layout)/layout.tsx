a19fcd29c2dd310dad6d00121e6ef84fea59fb72
hyungji.k <icthelight@naver.com>
2025-04-30 15:39:08 +0900

    ✨ #43 feat: intercept routing + parallel routing 적용

--- app/(with-layout)/layout.tsx
+++ app/(with-layout)/layout.tsx
@@ -1,10 +1,17 @@
 import { ReactNode } from "react";
 import Header from "../components/Header";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  modal,
+  children,
+}: {
+  modal: ReactNode;
+  children: ReactNode;
+}) {
   return (
     <>
       <Header />
+      {modal}
       {children}
     </>
   );
