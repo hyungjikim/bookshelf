d1883b6d3f6bb7ba13deee762e3a712ed377cb0d
hyungji.k <icthelight@naver.com>
2025-05-01 16:49:45 +0900

    ✨ #48 feat: 책 상세 페이지에 헤더, 본문 컴포너트 추가 - 헤더는 books 테이블, 본문은 book_details 테이블에서 데이터를 조회
    - books 조회를 위해 book_details의 응답값이 필요하므로, 헤더 UI를 Suspense로 감싸 병렬 렌더링을 구현
    
    - 각 UI의 로딩 상태를 분리해 보다 빠르고 유연한 화면 전환 제공

--- app/(with-layout)/@modal/(.)post/[id]/page.tsx
+++ app/(with-layout)/@modal/(.)post/[id]/page.tsx
@@ -1,5 +1,12 @@
 import { getBookDetail } from "@/app/lib/queries/getBookDetail";
 import { notFound } from "next/navigation";
+import { BookDetailModal } from "./components/BookDetailModal";
+import { Suspense } from "react";
+import { Overlay } from "@/app/components/Overlay";
+import { Header } from "../../../post/[id]/components/Header";
+import { Content } from "../../../post/[id]/components/Content";
+import { Loader } from "lucide-react";
+import * as stylex from "@stylexjs/stylex";
 
 export default async function Page({
   params,
@@ -13,5 +20,27 @@ export default async function Page({
     notFound();
   }
 
-  return <h1>intercept {id}</h1>;
+  return (
+    <Overlay>
+      <BookDetailModal>
+        <Suspense
+          fallback={
+            <div {...stylex.props(styles.fallbackWrapper)}>
+              <span>책 정보를 불러오고 있어요...</span>
+              <Loader />
+            </div>
+          }
+        >
+          <Header id={Number(bookDetail.book_id)} />
+        </Suspense>
+        <Content body={bookDetail.content} />
+      </BookDetailModal>
+    </Overlay>
+  );
 }
+
+const styles = stylex.create({
+  fallbackWrapper: {
+    minHeight: "56px",
+  },
+});
