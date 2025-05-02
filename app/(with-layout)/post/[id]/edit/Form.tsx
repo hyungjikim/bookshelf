8dee7ff1ea05e6adb6ce406d2b7254f241e7823c
hyungji.k <icthelight@naver.com>
2025-05-02 17:34:57 +0900

    ✨ #39 feat: 게시물 수정 폼

---
+++ app/(with-layout)/post/[id]/edit/Form.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import * as stylex from "@stylexjs/stylex";
+import { useActionState } from "react";
+import Form from "next/form";
+import EditorWrapper from "@/app/(without-layout)/new/components/editor/Tiptap";
+import { update } from "./actions";
+import { EditState } from "./types";
+import { formStyles } from "@/app/styles/form.styles";
+
+interface EditFormProps {
+  /** 수정할 게시물의 id (dynamic segment) */
+  id: number;
+  /** 수정할 게시물의 기존 내용 */
+  defaultValue: string;
+}
+
+export function EditForm({ defaultValue, id }: EditFormProps) {
+  const [state, formAction, pending] = useActionState<EditState, FormData>(
+    update,
+    {
+      message: "",
+    }
+  );
+
+  const [content, setContent] = useState(defaultValue);
+
+  return (
+    <Form action={formAction} {...stylex.props(formStyles.container)}>
+      <input type="hidden" name="id" value={id} />
+
+      <EditorWrapper value={content} onChange={setContent} />
+      <input type="hidden" name="content" value={content} />
+
+      {state?.message && (
+        <p aria-live="polite" role="status">
+          {state.message}
+        </p>
+      )}
+
+      <button type="submit" disabled={pending}>
+        수정하기
+      </button>
+    </Form>
+  );
+}
