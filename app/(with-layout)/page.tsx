0727e90998629423f4e0f90b99a892b6d1850c0b
hyungji.k <icthelight@naver.com>
2025-05-06 21:47:19 +0900

    ♻️ #71 refactor: fetchMoreBooks 함수 사용

--- app/(with-layout)/page.tsx
+++ app/(with-layout)/page.tsx
@@ -1,26 +1,17 @@
-import { createClient } from "@/utils/supabase/server";
 import * as stylex from "@stylexjs/stylex";
-import { PAGE_SIZE } from "../constants/books";
-import { mapToBookUI } from "../utils/mapBooks";
-import { BOOKS_SELECT } from "../lib/queries/getBooks";
 import InfiniteBookshelf from "./components/bookshelf/InfiniteBookshelf";
+import { fetchMoreBooks } from "./actions/loadMoreBooks";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const { data: initialData } = await supabase
-    .from("book_details")
-    .select(BOOKS_SELECT)
-    .limit(PAGE_SIZE);
+  const initialData = await fetchMoreBooks(0);
 
   if (!initialData)
     return <p {...stylex.props(styles.noBooks)}>📖 아직 추가된 책이 없어요!</p>;
 
-  const mappedBooks = initialData.map(mapToBookUI);
-
   return (
     <div>
       <main {...stylex.props(styles.wrapper)}>
-        <InfiniteBookshelf initialData={mappedBooks} />
+        <InfiniteBookshelf initialData={initialData} />
       </main>
     </div>
   );
