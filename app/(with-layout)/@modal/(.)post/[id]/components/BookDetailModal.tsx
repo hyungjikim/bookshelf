edaab0c24532f6cf4d0597167e77cd54b88ead2c
hyungji.k <icthelight@naver.com>
2025-05-07 21:08:02 +0900

    💄 #44 feat: UI 스타일 수정

--- app/(with-layout)/@modal/(.)post/[id]/components/BookDetailModal.tsx
+++ app/(with-layout)/@modal/(.)post/[id]/components/BookDetailModal.tsx
@@ -4,19 +4,18 @@ import { ReactNode } from "react";
 export function BookDetailModal({ children }: { children: ReactNode }) {
   return (
     <div role="dialog" aria-modal="true" {...stylex.props(styles.container)}>
-      <div {...stylex.props(styles.content)}>
-        <div>{children}</div>
-      </div>
+      <div {...stylex.props(styles.content)}>{children}</div>
     </div>
   );
 }
 
 const styles = stylex.create({
   container: {
-    maxWidth: "760px",
+    maxWidth: "460px",
+    minHeight: "700px",
     width: "100%",
-    padding: "24px",
     margin: "0 auto",
+    padding: "24px",
     backgroundColor: "white",
     borderRadius: "8px",
   },
@@ -26,14 +25,5 @@ const styles = stylex.create({
     gap: "16px",
     maxHeight: "80vh",
     overflow: "auto",
-  },
-  buttonWrapper: {
-    position: "sticky",
-    top: 0,
-    display: "flex",
-    justifyContent: "end",
-    gap: "8px",
-    padding: "8px",
-    backgroundColor: "white",
   },
 });
