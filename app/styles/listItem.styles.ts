3687f58634bc8d8948686b8af0937c9ab4c433aa
hyungji.k <icthelight@naver.com>
2025-05-09 22:21:35 +0900

    💄 #87 feat: 스타일 파일 업데이트

--- app/styles/listItem.styles.ts
+++ app/styles/listItem.styles.ts
@@ -1,12 +1,15 @@
 import * as stylex from "@stylexjs/stylex";
+import { tokens } from "./tokens.stylex";
 
 export const listItemStyles = stylex.create({
   li: {
     border: "1px solid #ccc",
     borderRadius: "4px",
     cursor: "pointer",
-    ":hover": {
-      backgroundColor: "#ccc",
+
+    backgroundColor: {
+      default: tokens.white,
+      ":hover": tokens.primary,
     },
   },
   liInner: {
