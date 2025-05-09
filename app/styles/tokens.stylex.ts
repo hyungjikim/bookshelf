6b4304f41bbbc58c253cbd7d6499c6b426399cc6
hyungji.k <icthelight@naver.com>
2025-05-09 15:33:20 +0900

    💄 #83 feat: 토큰 생성

---
+++ app/styles/tokens.stylex.ts
@@ -0,0 +1,10 @@
+import * as stylex from "@stylexjs/stylex";
+
+export const tokens = stylex.defineVars({
+  primary: "#D9EAFD",
+  secondary: "#9AA6B2",
+  tertiary: "#BCCCDC",
+  dark: "#9AA6B2",
+  text: "#0F172A",
+  white: "#F8FAFC",
+});
