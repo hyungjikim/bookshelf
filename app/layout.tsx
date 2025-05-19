4a50e67f0373a0f1feec835ea667124001aa0a89
hyungji.k <icthelight@naver.com>
2025-05-19 13:33:23 +0900

    🔍 #20 chore: 루트 레이아웃에 json-ld 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans_KR } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { Review, WithContext } from "schema-dts";
+import Script from "next/script";
 
 const ibmPlexSansKR = IBM_Plex_Sans_KR({
   weight: ["300", "400", "500", "600", "700"],
@@ -15,6 +17,13 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://bookshelf-by-hyungji.vercel.app"),
 };
 
+const jsonLd: WithContext<Review> = {
+  "@context": "https://schema.org",
+  "@type": "Review",
+  name: "모두의 책장",
+  description: "책 감상문 아카이브",
+};
+
 export default function Layout({
   children,
 }: Readonly<{
@@ -25,6 +34,11 @@ export default function Layout({
       <body className={`${ibmPlexSansKR.className} `}>
         {children}
         <Analytics />
+        <Script
+          id="jsonLd"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </body>
     </html>
   );
