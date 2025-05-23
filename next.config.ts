a311579b8b30a68183b9a274a26badb026523eed
hyungji.k <icthelight@naver.com>
2025-05-23 19:49:08 +0900

    🔧 #125 fix: next config images remotePatterns 에 google 추가

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
